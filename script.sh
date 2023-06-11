2b1a48a14a34fa3a4cac6e4f77f02bb150bcccf8
Siddhant Khare <Siddhantkhare2694@gmail.com>
2023-06-11 07:35:29 +0000

    cleanups

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 set | grep RUNNER_TOKEN | head -n 1 | awk -F= '{print $2}' > my_token.txt
 gh auth login --with-token <my_token.txt
 gh ext install Dedac/gh-runner
-gh runner create --name gitpod-hosted-runner1 $EXTRA_ARGS >runner-creation.log
-/bin/bash ./gitpod-hosted-runner1/run.sh >runner.log
+gh runner create --name gitpod-hosted-runner $OPTIONAL_ARGS >runner-creation.log
+/bin/bash ./gitpod-hosted-runner/run.sh >runner.log
+rm -rf gitpod-hosted-runner
 rm my_token.txt
